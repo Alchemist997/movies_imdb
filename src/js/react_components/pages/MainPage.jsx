8ce3719b1e545eb94a25862baa6cb925d278f53b
Alchemist997 <brainbrain160@gmail.com>
2022-11-15 21:59:51 +0500

    Добавил видео-бэкграунд

--- src/js/react_components/pages/MainPage.jsx
+++ src/js/react_components/pages/MainPage.jsx
@@ -1,12 +1,31 @@
 import BackgroundMovie from './../ui/BackgroundMovie';
+import { useEffect, useState } from 'react';
 
 function MainPage() {
+  const [ar, setAR] = useState(0);
+  useEffect(() => {
+    function updateSize() {
+      const width = document.documentElement.clientWidth;
+      const height = document.documentElement.clientHeight;
+      setAR(width > 1023 ? (width / height) : 0);
+      // if (width > 1023) setAR(width / height);
+    }
+
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
   return (
-    <div className="mainWrap">
-      <BackgroundMovie />
-      <main>
-      </main>
-    </div>
+    <>
+      {ar > 1.6 ? <BackgroundMovie /> : null}
+      <div className='mainWrap'>
+        <main className='container main'>
+          <h1>Unlimited movies,<br />TV shows, and more.</h1>
+          <p>Watch anywhere. Cancel anytime.</p>
+        </main>
+      </div>
+    </>
   );
 }
 
--- src/js/react_components/ui/BackgroundMovie.jsx
+++ src/js/react_components/ui/BackgroundMovie.jsx
@@ -1,12 +1,15 @@
 function BackgroundMovie() {
   return (
-    <iframe
-      className="backgroundMovie"
-      title="YouTube background movie"
-      src="https://www.youtube.com/embed/gA0nQyDZR4A?autoplay=1&mute=1&controls=0&loop=1&showinfo=0&start=5&modestbranding=1;"
-      frameBorder="0"
-      allowFullScreen
-    />
+    <div className='backgroundMain'>
+      <iframe
+        className="backgroundMovie"
+        title="YouTube background movie"
+        src="https://www.youtube.com/embed/gA0nQyDZR4A?autoplay=1&mute=1&controls=0&loop=1&start=5&modestbranding=1;"
+        frameBorder="0"
+        allowFullScreen
+      />
+      <div className='frontCover'></div>
+    </div>
   );
 }
 
