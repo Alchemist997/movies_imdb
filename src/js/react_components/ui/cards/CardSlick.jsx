7640b0f1e1a83f8eff7dcfc38a4759feb546c24d
Alchemist997 <brainbrain160@gmail.com>
2023-02-03 19:11:38 +0500

    Добавил: отмену перехода по ссылке при перетаскивании в слайдере, onPlay-предупреждение

--- src/js/react_components/ui/cards/CardSlick.jsx
+++ src/js/react_components/ui/cards/CardSlick.jsx
@@ -4,9 +4,18 @@ import SVG from '../svg/SVGList';
 
 function CardSlick({ title, movieID, rate, imgSrc }) {
   const [imgIsNotLoaded, setImgIsNotLoaded] = useState('notLoaded');
+  const [pointerInfo, setPointerInfo] = useState('');
 
   return (
-    <Link to={`/movie/${movieID}`} className='card-slick' draggable='false'>
+    <Link to={`/movie/${movieID}`}
+      className='card-slick'
+      draggable='false'
+      onPointerDown={evt => setPointerInfo(`${evt.clientX}${evt.clientY}`)}
+      onClick={evt => {
+        if (pointerInfo === `${evt.clientX}${evt.clientY}`) return;
+        evt.preventDefault();
+      }}
+    >
       <div className={`card-slick__img-wrap ${imgIsNotLoaded}`}>
         <img className='card-slick__movieCover'
           src={imgSrc}
