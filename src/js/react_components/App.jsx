bacc7363655bea1dd3547feb3f857ec56d0b1660
Alchemist997 <brainbrain160@gmail.com>
2023-02-07 22:16:45 +0500

    Добавил gh-pages

--- src/js/react_components/App.jsx
+++ src/js/react_components/App.jsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import MoviePage from './pages/MoviePage';
 import ErrorPage from './pages/ErrorPage';
-
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='*' element={<ErrorPage />} />
         <Route path='movie/:movieID' element={<MoviePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
